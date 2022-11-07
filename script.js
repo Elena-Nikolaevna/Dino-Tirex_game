44ec15cb3ef5e7475927c70c449c5692c4ba1630
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-08 01:06:56 +0200

    Update script.js
    score

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-import {  updateGround, setupGround } from "./ground.js";
+import { updateGround, setupGround } from "./ground.js";
 
 const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
+const SPEED_SCALE_INCREASE = 0.00001
 
 const worldElem = document.querySelector("[data-world]");
+const scoreElem = document.querySelector("[data-score]")
 //console.log(worldElem)
 
 setPixelToWorldScale();
 window.addEventListener("resize", setPixelToWorldScale);
+document.addEventListener("keydown", handleStart, { once: true });
 
-setupGround();
+//setupGround();
 
 let lastTime;
+let speedScale
+let score
 function update(time) {
   if (lastTime == null) {
     lastTime = time;
@@ -19,14 +24,33 @@ function update(time) {
     return;
   }
   const delta = time - lastTime;
-  console.log(delta);
+  //console.log(delta);
 
-  updateGround(delta, 1);
+  updateGround(delta, speedScale);
+  updateSpeedScale(delta)
+  updateScore(delta)
 
   lastTime = time;
   window.requestAnimationFrame(update);
 }
-window.requestAnimationFrame(update);
+//window.requestAnimationFrame(update);
+
+function updateSpeedScale(delta) {
+  speedScale += delta * SPEED_SCALE_INCREASE
+}
+
+function updateScore(delta) {
+  score += delta * 0.01
+  scoreElem.textContent = Math.floor(score)
+}
+
+function handleStart() {
+  lastTime = null;
+  speedScale = 1;
+  score = 0
+  setupGround();
+  window.requestAnimationFrame(update);
+}
 
 function setPixelToWorldScale() {
   let worldToPixelScale;
